7719ff24cba1e167a01dabddedb93f37ac94cd14
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-14 18:12:04 +0530

    Update fetchMessages.ts

--- utils/fetchMessages.ts
+++ utils/fetchMessages.ts
@@ -1,9 +1,13 @@
 import { Message } from "../typings";
+//importing the Message interface from typings.ts
 
 const fetcher = async () => {
+  
   const res = await fetch("/api/getMessages");
+  //api endpoint to get messages from upstash
   const data = await res.json();
-
   const messages: Message[] = data.messages;
+  //messages is an array of Message type
   return messages;
 };
+export default fetcher;
