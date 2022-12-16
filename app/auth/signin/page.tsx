76792d9d291e3c257bf453f746e77affef0f1085
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-16 22:55:35 +0530

    Create page.tsx

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,22 @@
+import { getProviders } from "next-auth/react";
+import Image from "next/image";
+import SignInComponent from "./signin";
+async function SignIn() {
+  const providers = await getProviders();
+  return (
+    <div className="flex ">
+      <div>
+        <Image
+          className="rounded-full mx-2 object-cover"
+          src="https://links.papareact.com/161"
+          width={700}
+          height={700}
+          alt="Picture"
+        />
+      </div>
+      <SignInComponent providers={providers} />
+    </div>
+  );
+}
+
+export default SignIn;
