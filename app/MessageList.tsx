f48d935a52c6c735202c54ff9dbc70dcd85d3cc0
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-16 06:20:28 +0530

    Update MessageList.tsx

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import fetcher from "../utils/fetchMessages";
+import useSWR from "swr";
+import MessageComponent from "./MessageComponent";
+import { clientPusher } from "../pusher";
+import { Message } from "../typings";
 
 function MessageList() {
+  const { data: messages, error, mutate } = useSWR("/api/getMessages", fetcher);
+
+ 
+
   return (
-    <div>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
-      <p>Message</p>
+    <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl mx-auto">
+      {messages?.map((message) => (
+        <MessageComponent key={message.id} message={message} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default MessageList
+export default MessageList;
