f857d4bc43faf6088db4aa38de081957494294fb
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-17 00:06:22 +0530

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-export function Providers({ children }: any) {
-  return <SessionProvider>{children}</SessionProvider>;
+export function Providers({ children, session }: any) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
