3e5c1d1eda923d567d9400a32aec20429466179a
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-16 06:20:53 +0530

    Update MessageList.tsx

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -9,7 +9,20 @@ import { Message } from "../typings";
 function MessageList() {
   const { data: messages, error, mutate } = useSWR("/api/getMessages", fetcher);
 
- 
+  //useEffect for updating the list of messages
+  useEffect(() => {
+    const channel = clientPusher.subscribe("messages");
+    //subscribe to the messages channel
+    //Before your web app can receive the event you publish, your web app needs to subscribe to the messages channel. Do this with pusher.subscribe
+    channel.bind("new-message", async (data: Message) => {
+      mutate(fetcher, {
+        optimisticData: [data, ...messages!],
+        //optimisticData is the data that will be shown to the user before the data is actually fetched from the server and is used to make the UI feel more responsive to the user
+        rollbackOnError: true,
+        //rollbackOnError is used to rollback the optimisticData if there is an error
+      });
+    });
+  }, [messages, mutate, clientPusher]);
 
   return (
     <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl mx-auto">
