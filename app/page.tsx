9badeb739129e0b8a3240f4e9f51aff98203cb4d
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-17 00:06:48 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { Message } from "../typings";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
+import { unstable_getServerSession } from "next-auth/next";
+import { Providers } from "./providers";
+//Creating server side
+//then we swap it with what users sees on the client side to make it more quick
+async function page() {
+  const data = await fetch(
+    `${process.env.VERCEL_URL || "http://localhost:3000"}/api/getMessages`
+  ).then((res) => res.json());
+  const messages: Message[] = data.messages;
+  const session = await unstable_getServerSession();
 
-function page() {
+  // console.log(data);
   return (
-    <div>
-      <h1>Welcome to Meta Messenger</h1>
-      {/* messages */}
-      {/* message input */}
-      <MessageList />
-      <MessageInput />
-    </div>
+    <Providers session={session}>
+      <main>
+        {/* messages */}
+        {/* message input */}
+        <MessageList initialMessages={messages} />
+        <MessageInput session={session} />
+      </main>
+    </Providers>
   );
 }
 
