ae108f7e0357da167bb7c70d8429defe0d297bbe
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-17 20:28:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["links.papareact.com"],
+    domains: ["links.papareact.com","scontent.fbom3-1.fna.fbcdn.net" ],
   },
   experimental: {
     appDir: true,
