046027c822a70fc5cb761bcd0ed386046f95feb0
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-17 20:32:43 +0530

    Update LogoutButton.tsx

--- app/LogoutButton.tsx
+++ app/LogoutButton.tsx
@@ -1,9 +1,14 @@
-"use client"
+"use client";
+import { signOut } from "next-auth/react";
 
 function LogoutButton() {
   return (
-    <button className=" bg-blue-500 hover:bg-blue-700 px-4 text-white py-2 rounded font-bold" 
-    onClick={()=>{console.log("happy")}}>
+    <button
+      className=" bg-blue-500 hover:bg-blue-700 px-4 text-white py-2 rounded font-bold"
+      onClick={() => {
+        signOut();
+      }}
+    >
       Sign out
     </button>
   );
