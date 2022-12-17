55e6814478f9efc3a58db70c9c6c338714052d8c
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-17 20:30:31 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = { matcher: ["/"] };
+//"/" denotes the url that is being accessed here being the home page
+//person is redirected to sign in page if not signed in as soon as they try to access the page
