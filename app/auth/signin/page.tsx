bceb56f9b73932831e8d97d2aff99458e2f96106
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-17 20:35:54 +0530

    Update page.tsx

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -4,10 +4,11 @@ import SignInComponent from "./signin";
 async function SignIn() {
   const providers = await getProviders();
   return (
-    <div className="flex ">
+   
+    <div className="flex justify-center flex-col items-center ">
       <div>
         <Image
-          className="rounded-full mx-2 object-cover"
+          className="rounded-full mx-2 object-cover "
           src="https://links.papareact.com/161"
           width={700}
           height={700}
