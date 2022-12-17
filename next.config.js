69bc3f63b60dc439abaee4b243e05efb700226b1
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-17 23:43:26 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["links.papareact.com","scontent.fbom3-1.fna.fbcdn.net" ],
+    domains: ["links.papareact.com","scontent.fbom3-1.fna.fbcdn.net","localhost" ],
+   // Got an error Invalid src prop ('here is a link') on `next/image`
+   // to resolve this issue I am adding "localhost" to the domains 
   },
   experimental: {
     appDir: true,
