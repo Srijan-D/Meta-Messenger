a2f18d00435bfbbeac34f761b7e767f1ec49c237
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-12-17 20:35:41 +0530

    Create head.tsx

---
+++ app/auth/signin/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Meta messenger SignIn❤</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="https://links.papareact.com/jne" />
+    </>
+  );
+}
